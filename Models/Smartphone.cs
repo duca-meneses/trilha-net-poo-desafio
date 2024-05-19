e55d6031221bbf2ab59cc8a27927842936043257
Carlos Eduardo <ducameneses16@gmail.com>
2024-05-19 00:58:55 -0300

    feat: Implementando os TODOs do Desafio .Net POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,29 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models;
+
+public abstract class Smartphone
 {
-    public abstract class Smartphone
+    public Smartphone(string numero, string _modelo, string _imei, int _memoria)
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        Numero = numero;
+        modelo = _modelo;
+        imei = _imei;
+        memoria = _memoria;
     }
+    public string Numero { get; set; }
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+    private string modelo;
+    private string imei;
+    private int memoria;
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+
+Smartphone nokia = new Nokia(numero: "12233546", _modelo: "Modelo 1", _imei: "123218111", _memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("ChatGPt");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+
+Smartphone iphone = new Iphone(numero: "090933546", _modelo: "Modelo 10", _imei: "001113333", _memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
